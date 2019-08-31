0f821eb42f92397414e53266ff6d38e1da07d906
Abhi Mohnani <amohnan1@jhu.edu>
2019-08-31 19:20:49 -0400

    Completed files first.c, second.c and third,c for homework 0.

---
+++ homework/hw0/first.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+  printf("The first prize goes to Harry\n");
+  return 0;
+}
---
+++ homework/hw0/second.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+  printf("The second prize goes to Hermione\n");
+  return 0;
+}
